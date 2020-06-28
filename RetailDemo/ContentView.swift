a8ae27a160fd3871e360f8d6c3878513e29f8002
Steve Cotner <stevecotner@gmail.com>
2020-06-28 12:15:54 -0700

    Initial Commit

---
+++ RetailDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RetailDemo
+//
+//  Created by Stephen E. Cotner on 6/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RetailDemo/RetailDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetailDemoApp.swift
+//  RetailDemo
+//
+//  Created by Stephen E. Cotner on 6/28/20.
+//
+
+import SwiftUI
+
+@main
+struct RetailDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
